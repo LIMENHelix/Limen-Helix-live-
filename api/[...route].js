e54bd37347bb533a9d5d0965a47a12dfec46c2bb
LIMENHelix <chrishubbel72@gmail.com>
2026-08-17 19:52:11 -0500

    Route the authoritative brain-v2 read endpoint

--- api/[...route].js
+++ api/[...route].js
@@ -52,6 +52,7 @@ const HANDLERS = {
   'asset-quote': require('../handlers/asset-quote'),
   'biosensor-state': require('../handlers/biosensor-state'),
   'brain-cognition': require('../handlers/brain-cognition'),
+  'domain-brain': require('../handlers/domain-brain'),
   'brain-shadow': require('../handlers/brain-shadow'),
   'brain-cognition-refresh': require('../handlers/brain-cognition-refresh'),
   'capital-engine': require('../handlers/capital-engine'),
