5cdf6c14f0eedc7590f90c8210cb21b255838d1c
Chris Hubbel <chrishubbel72@gmail.com>
2026-08-23 20:06:36 -0500

    Carry NY Fed effective date into observations

--- scripts/test-upstream-observation-identities.js
+++ scripts/test-upstream-observation-identities.js
@@ -21,6 +21,9 @@ global.fetch = async function (url) {
   else if (text.indexOf('/accounting/dts/') >= 0) body = { data: [
     { record_date: '2026-08-20', account_type: 'Treasury General Account (TGA) Opening Balance', close_today_bal: 'null', open_today_bal: '936406' }
   ] };
+  else if (text.indexOf('markets.newyorkfed.org') >= 0) body = { refRates: [
+    { effectiveDate: '2026-08-20', percentRate: 3.63 }
+  ] };
   else body = [{ page: 1 }, [{ value: 2, date: '2025' }, { value: 1, date: '2024' }]];
   return { ok: true, status: 200, json: async function () { return body; } };
 };
@@ -38,11 +41,13 @@ global.fetch = async function (url) {
       H._fetchWorldBankInfra, H._fetchWorldBankFoodIndex
     ];
     var treasury = [H._fetchTreasuryMTS, H._fetchTreasuryOperatingCash];
+    var nyFed = H._fetchNYFedEFFR;
     for (var i = 0; i < fred.length; i++) assert.equal((await fred[i]()).sourceUpdatedAt, '2026-08-01');
     for (var j = 0; j < wb.length; j++) assert.equal((await wb[j]()).sourceUpdatedAt, '2025');
     assert.equal((await treasury[0]()).sourceUpdatedAt, '2026-07-31');
     assert.equal((await treasury[1]()).sourceUpdatedAt, '2026-08-20');
-    console.log('15/15 passed');
+    assert.equal((await nyFed()).sourceUpdatedAt, '2026-08-20');
+    console.log('16/16 passed');
   } finally {
     global.fetch = originalFetch;
     if (originalFred === undefined) delete process.env.FRED_API_KEY;
